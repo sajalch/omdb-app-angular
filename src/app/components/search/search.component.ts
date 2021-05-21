fefbb2e661a2e41f2b0d6e050a1db0f01f3927f1
sajalch <sajalchakraborty7@gmail.com>
2021-05-21 19:20:13 +0530

    add fire search on enter press

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output,EventEmitter,Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -9,17 +9,20 @@ export class SearchComponent implements OnInit {
   @Output() handleFilter:EventEmitter<any>= new EventEmitter();
   isFilter:Boolean=false;
   searchVal:String="";
-  constructor(private route:ActivatedRoute) { 
+  constructor(private route:ActivatedRoute,private router: Router) { 
   
   }
   searchQuery:String;
   currentYear: any = new Date().getFullYear();
-  handleOnSearch(val){
+  handleOnSearch(event:any="",val){
     if(val){
     this.searchQuery="/search/"+val+"/page/1"; 
     this.searchVal=val;   
     }else{
       this.searchQuery="/"; 
+    }
+    if(event.key === "Enter"){
+      this.searchNow();
     }
   }
   
@@ -27,11 +30,15 @@ export class SearchComponent implements OnInit {
     this.isFilter=val;  
     this.handleFilter.emit(val);
   }
+
+  searchNow(){
+    this.router.navigate(["/search/"+this.searchVal+"/page/1"]);
+  }
   ngOnInit(): void {
     // console.log(this.route.snapshot)
     if(this.route.snapshot.params.search){
       this.searchVal=this.route.snapshot.params.search;
-      this.handleOnSearch(this.searchVal);
+      this.handleOnSearch("",this.searchVal);
     }
   }
 
